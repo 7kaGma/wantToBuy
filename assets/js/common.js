92b891516f8e631812f52c9a1f7c4cb92c742de1
7kaGma <trlm7ka2ma80@gmail.com>
2024-05-02 14:01:53 +0900

    微修正

--- assets/js/common.js
+++ assets/js/common.js
@@ -47,30 +47,32 @@
   })
 
 let valueId=[];
-console.dir(valueId);
 
-  for (let i = 0; i < localStorage.length; i++) {
-    let  key = localStorage.key(i);
-    let  valueInput = localStorage.getItem(key);
+
+let judgeframe = document.querySelectorAll(".origin"); // ループの外で一度だけ取得
+
+for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let valueInput = localStorage.getItem(key);
     let value = JSON.parse(valueInput);
     /*フレームの生成*/
-    let divFrame =document.createElement("div");
-    divFrame.className="frame";
+    let divFrame = document.createElement("div");
+    divFrame.className = "frame";
 
-    let itemtag =document.createElement("h3");
-    itemtag.className="item";
-    itemtag.textContent =value.item;
-    
-    let imageBox =document.createElement("figure");
-    imageBox.className="imageBox";
+    let itemtag = document.createElement("h3");
+    itemtag.className = "item";
+    itemtag.textContent = value.item;
 
-    let imageElement =document.createElement("img");
-    imageElement.className="image";
+    let imageBox = document.createElement("figure");
+    imageBox.className = "imageBox";
+
+    let imageElement = document.createElement("img");
+    imageElement.className = "image";
     imageElement.src = value.image;
 
-    let priceCard =document.createElement("p");
-    priceCard.className="price";
-    priceCard.textContent =`${value.price}円`;
+    let priceCard = document.createElement("p");
+    priceCard.className = "price";
+    priceCard.textContent = `${value.price}円`;
 
     imageBox.appendChild(imageElement);
     divFrame.appendChild(itemtag);
@@ -78,5 +80,17 @@ console.dir(valueId);
     divFrame.appendChild(priceCard);
     document.getElementById("shoppingBasket").appendChild(divFrame);
 
-    valueId.push(value.item)
-  }
+    valueId.push(value.item);
+}
+
+judgeframe.forEach(function(element) {
+    let itemName = element.querySelector(".item").textContent; // テキスト内容を取得する
+    let btnName = element.querySelector(".btn");
+    let cancelName = element.querySelector(".cancel");
+
+    if (valueId.includes(itemName)) {
+        btnName.classList.add("hidden");
+        cancelName.classList.add("appear");
+    }
+});
+
